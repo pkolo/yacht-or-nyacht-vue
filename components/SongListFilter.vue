c9973638909ff3344ff7a4b65243ec1e9a28ecdc
Patrick Kolodgy <pkolodgy@gmail.com>
2017-11-10 09:27:46 -0500

    Style filter input

--- components/SongListFilter.vue
+++ components/SongListFilter.vue
@@ -1,5 +1,7 @@
 <template>
-  <input ref="input" :value="value" v-on:input="updateFilter($event.target.value)"/>
+  <div class="song-list-filter">
+    <input placeholder="Filter by artist or title..." ref="input" :value="value" v-on:input="updateFilter($event.target.value)"/>
+  </div>
 </template>
 
 <script>
@@ -16,4 +18,22 @@
 </script>
 
 <style>
+  .song-list-filter {
+    width: 50%;
+    margin: 20px auto;
+  }
+
+  .song-list-filter > input {
+    width: 100%;
+    height: 2em;
+    text-align: center;
+    font-family: "Lato", sans-serif;
+    font-size: 1.5em;
+    color: #444;
+    padding: 10px;
+  }
+
+  .song-list-filter > input::-webkit-input-placeholder {
+    color: #ababab;
+  }
 </style>
