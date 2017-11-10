df895db48ce274ed178e4c7811d20238327d151c
Patrick Kolodgy <pkolodgy@gmail.com>
2017-11-09 20:05:53 -0500

    Headers sorting list

--- components/SongList.vue
+++ components/SongList.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="song-list">
-    <a href="#" @click="songSort('yachtski')">Yachtski</a>
-    <a href="#" @click="songSort('title')">Title</a>
+    <div class="song-list-header">
+      <a href="#" @click="songSort('yachtski')">Yachtski</a>
+      <a href="#" @click="songSort('title')">Title</a>
+    </div>
     <div class="song" v-for="song in songs">
       <song-bar :song="song" />
     </div>
@@ -23,10 +25,10 @@
     methods: {
       songSort (column) {
         if (this.sortOrder === 'desc') {
-          this.songs = this.songs.sort((s1, s2) => s1[column] - s2[column])
+          this.songs = this.songs.sort((s1, s2) => s1[column] > s2[column] ? 1 : -1)
           this.sortOrder = 'asc'
         } else {
-          this.songs = this.songs.sort((s1, s2) => s2[column] - s1[column])
+          this.songs = this.songs.sort((s1, s2) => s1[column] < s2[column] ? 1 : -1)
           this.sortOrder = 'desc'
         }
       }
@@ -42,5 +44,9 @@
 </script>
 
 <style>
-
+  .song-list-header {
+    display: grid;
+    grid-template-columns: 1fr 6fr repeat(5, 1fr);
+    margin-bottom: 5px;
+  }
 </style>
