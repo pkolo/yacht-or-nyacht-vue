2e9aec3d58b20ac0edc8482915532d000db04a8b
Patrick Kolodgy <pkolodgy@gmail.com>
2017-11-12 10:46:33 -0500

    Create personnel view

---
+++ pages/personnel/_slug.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>{{ personnel.name }}</p>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    asyncData ({ params }) {
+      let id = params.slug.match(/\d+/)
+      return axios.get(`http://localhost:3000/api/v1/personnel/${id}`)
+        .then((res) => {
+          return { personnel: res.data }
+        })
+    }
+  }
+</script>
+
+<style>
+</style>
