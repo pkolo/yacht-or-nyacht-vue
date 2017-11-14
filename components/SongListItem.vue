6388702a0528707df6790a9156f1280ce70523eb
Patrick Kolodgy <pkolodgy@gmail.com>
2017-11-13 21:50:29 -0500

    Improve link generation and formatting

--- components/SongListItem.vue
+++ components/SongListItem.vue
@@ -2,8 +2,17 @@
   <div class="song-list-item" :style="{ backgroundColor: this.getColor(song.yachtski) }">
     <div>{{ song.yachtski | roundNum }}</div>
     <div>
-      <span v-html="this.$options.filters.artistURL(song.artists, song.featured_artists)"></span> -
-      <a :href="song.url">{{ song.title }}</a>
+      <div class="artist-links">
+        <span v-for="artist in song.artists">
+          <nuxt-link :to="{ path: `personnel/${artist.id}`}">{{ artist.name }}</nuxt-link>
+        </span>
+      </div>
+      <div class="artist-links featured" v-if="song.featured_artists.length > 0">
+        <span v-for="artist in song.featured_artists">
+          <nuxt-link :to="{ path: `personnel/${artist.id}`}">{{ artist.name }}</nuxt-link>
+        </span>
+      </div>
+      <nuxt-link class="song-title" :to="{ path: `songs/${song.id}`}">{{ song.title }}</nuxt-link>
     </div>
     <div>{{ song.year }}</div>
     <div :style="{ backgroundColor: this.getColor(song.scores.jd) }">{{ song.scores.jd }}</div>
@@ -22,14 +31,6 @@
       song: Object
     },
     filters: {
-      artistURL: function (artists, featuredArtists) {
-        let artistList = artists.map(a => `<a href=${a.url}>${a.name}</a>`).join(', ')
-        if (featuredArtists.length > 0) {
-          let featuredArtistList = featuredArtists.map(a => `<a href=${a.url}>${a.name}</a>`).join(', ')
-          return `${artistList} w/ ${featuredArtistList}`
-        }
-        return artistList
-      },
       roundNum: function (number) {
         return number.toFixed(2)
       }
@@ -47,5 +48,24 @@
 
   .song-list-item > * {
     padding: 10px;
+  }
+
+  .artist-links {
+    display: inline-block;
+  }
+
+  .artist-links > * + *:before {
+    content: ", ";
+    white-space: pre;
+  }
+
+  .featured:before {
+    content: " w/ ";
+    white-space: pre;
+  }
+
+  .song-title:before {
+    content: " - ";
+    white-space: pre;
   }
 </style>
