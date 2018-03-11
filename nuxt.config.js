f75b2f9de720e8382252d515ab7200ca2e6221a0
Patrick Kolodgy <pkolodgy@gmail.com>
2018-03-11 10:23:29 -0400

    Add analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,6 +24,12 @@ module.exports = {
   plugins: [
     { src: '~plugins/vue-slider-component.js', ssr: false }
   ],
+  modules: [
+    // Simple usage
+    ['@nuxtjs/google-analytics', {
+      id: 'UA-100779074-1'
+    }]
+  ],
   build: {
     /*
     ** Run ESLint on save
