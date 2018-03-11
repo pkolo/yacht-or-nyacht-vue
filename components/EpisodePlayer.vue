5bb6b2f53ad5b9e266d45f5f884e903acb6a147d
Patrick Kolodgy <pkolodgy@gmail.com>
2018-03-11 10:23:46 -0400

    Create player component

---
+++ components/EpisodePlayer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="content-section">
+    <div class="podcast-player">
+      <div class="art19-web-player awp-medium awp-theme-light-blue" :data-primary-color="color" :data-episode-id="episode.data_id"></div>
+    </div>
+
+    <div class="air-date">
+      <span>Originally aired {{episode.air_date}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      episode: Object,
+      color: String
+    }
+  }
+</script>
+
+<style>
+  .air-date {
+    margin-top: -15px;
+    text-align: right;
+    font-size: 14px;
+    color: #999;
+  }
+</style>
