f8d52e5a13444b832066a17ba6bf722047a4c8bb
pkolo <pkolodgy@gmail.com>
2018-07-12 00:21:40 -0400

    Improve login page

--- pages/admin/login/index.vue
+++ pages/admin/login/index.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="container">
-    <form @submit.prevent="login">
-      <input type="text" placeholder="username" v-model="formData.name" />
-      <input type="password" placeholder="password" v-model="formData.password" />
-      <button type="submit">Submit</button>
+  <div class="login-container">
+    <form @submit.prevent="login" class="login-form left">
+      <div>
+        <p>Username</p>
+        <input type="text" placeholder="username" v-model="formData.name" />
+      </div>
+
+      <div>
+        <p>Password</p>
+        <input type="password" placeholder="password" v-model="formData.password" />
+      </div>
+
+      <div class="submit-button">
+        <button type="submit">Login</button>
+      </div>
     </form>
     <div class="errors" v-if="errors.length > 0">
       <div class="error-msg" v-for="error in errors">
@@ -53,4 +63,36 @@
 </script>
 
 <style>
+
+  .login-container {
+    margin: 40px auto;
+    max-width: 400px;
+    text-align: center;
+  }
+
+  .login-form {
+    padding: 40px;
+    border: 1px solid slategrey;
+    border-radius: 2px;
+  }
+
+  .left {
+    text-align: left;
+  }
+
+  p {
+    margin-bottom: 10px;
+  }
+
+  input {
+    margin-bottom: 20px;
+    padding: 10px;
+    width: 300px;
+    font-size: 16px;
+  }
+
+  button {
+    padding: 10px;
+    font-size: 16px;
+  }
 </style>
